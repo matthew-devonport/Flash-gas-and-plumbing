aca26883ec490a5b4c34870c96e85807640af599
matthew-devonport <roarsintheeast@gmail.com>
2020-06-22 14:55:08 +1200

    made the footer better with flex

--- client/components/Footer.jsx
+++ client/components/Footer.jsx
@@ -10,16 +10,20 @@ class Footer extends React.Component {
             <React.Fragment>
                 <div id='footer'>
                     <footer>
+                        <div className="footerFlex">
                         <div className="leftFooter">
                             <h1 className="contactInfo">CONTACT INFO</h1>
                             <a href="tel:0272435274"><img src="./img/phone-number.png" alt="Phone logo" className="phoneLogo"></img> </a>
                     <a href="mailto:flashgasandplumbing@gmail.com"><img src="./img/email.png" alt="Emaillogo" className="emailLogo"></img></a>
                        </div>
-                       <ul className="footer-nav">
+                       <div className="footer-nav">
                        <h1 className="socialmedia">SOCIAL MEDIA</h1>
-                            <li><Link><img src="./img/facebook.png" alt="Facebook logo" className="footerLogo facebook"></img> </Link></li>
-                            <li><Link><img src="./img/instagram.png" alt="Instagram logo" className="footerLogo instagram"></img> </Link></li>
-                        </ul>
+                       <div className="socialLinks">
+                            <a><Link><img src="./img/facebook.png" alt="Facebook logo" className="footerLogo facebook"></img> </Link></a>
+                            <a><Link><img src="./img/instagram.png" alt="Instagram logo" className="footerLogo instagram"></img> </Link></a>
+                        </div>
+                        </div>
+                        </div>
                         <p className="copyrightText">
                             Copyright &copy; Flash Gas and Plumbing. All rights reserved.<br />
                             <a href="https://github.com/matthew-devonport" target="_blank" className="matty">Website Design &amp; Build by Matthew Devonport</a>
