0fcf6b7c869c297591af0a49e7d2199e4745a060
matthew-devonport <roarsintheeast@gmail.com>
2020-06-30 12:23:16 +1200

    formatted about page to desktop

--- client/components/About.jsx
+++ client/components/About.jsx
@@ -13,25 +13,27 @@ class About extends React.Component {
                 </div>
                 <div id="aboutArea">
                     <div>
-                        <div className="aboutItem centerImg"><img src="./img/about.png"></img></div>
-                        <div className="aboutItem centerImg">
-                        Introducing Campbell Winter,Certified Gasfitter,plumber and Director
-                        for the Company established in 2020.Campbell is no newcomer to the industry 
+                        <div className="aboutItem">
+                        Introducing Campbell Winter. Certified Gasfitter, plumber and Director
+                        for the Company established in 2020.<br /><br /> 
+                        Campbell is no newcomer to the industry 
                         having honed his skill set over 11 years to give both the balance of youthful 
-                        enthusiasm and experience in the field.The Company’s target market is for 
+                        enthusiasm and experience in the field. The Company’s target market is for 
                         customers with high expectations of  workmanship and communication and to deliver 
                         to them a value added service.</div>
                         <div className="aboutItem centerImg"><h1>WHY CHOOSE US?</h1></div>
-                        <div className="aboutItem centerImg">
-                        *personalised service where customer deals directly 
-                        with the business owner and tradesman
-                        * Campbell is a Certified Gasfitter
-                        *emaculate,guaranteed workmanship
-                        *ability to project manage and coordinate with other trades
-                        *respect for customers homes or place of business when working
-                        *carry out gas and plumbing warrant of fitnesses for homes and business premises
-                        *highest standards of personal appearance,use of PPE and WorkSafe practices
-                        *absolute integrity to ensure customer satisfaction</div>
+                        <div className="aboutItem">
+                          <ul>
+                        <li>Personalised service where customer deals directly 
+                        with the business owner and tradesman.</li><br /> 
+                        <li>Campbell is a Certified Gasfitter.</li><br /> 
+                        <li>Emaculate, guaranteed workmanship.</li><br /> 
+                        <li>Ability to project manage and coordinate with other trades.</li><br /> 
+                        <li>Respect for customers homes or place of business when working</li><br /> 
+                        <li>Carry out gas and plumbing warrant of fitnesses for homes and business premises.</li><br /> 
+                        <li>Highest standards of personal appearance, use of PPE and WorkSafe practices.</li><br /> 
+                        <li>Absolute integrity to ensure customer satisfaction.</li><br /> 
+                        </ul></div>
                     </div>
                 </div>
                 <Footer />
